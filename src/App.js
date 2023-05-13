91bf3306254ba73e3d6abb124cd1108d9089f1c2
evramesh <evramesh88868@gmail.com>
2023-05-13 08:19:15 +0000

    rock

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Games from './components/Games'
+import './App.css'
+
+const choicesList = [
+  {
+    id: 'ROCK',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
+  },
+  {
+    id: 'SCISSORS',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
+  },
+  {
+    id: 'PAPER',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
+  },
+]
+
+const App = () => <Games send={choicesList} />
+
+export default App
